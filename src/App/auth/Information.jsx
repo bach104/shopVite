239c73a496cb19dfe9b242e7b2aae1462ecda2d2
bach104 <114276890+bach104@users.noreply.github.com>
2025-02-17 16:07:34 +0700

    update

--- src/App/auth/Information.jsx
+++ src/App/auth/Information.jsx
@@ -3,33 +3,7 @@ const Information = () => {
   return (
     <div className="mt-20 informationAuth">
       <ShowInformation/>
-      <div className="boxContainer pb-10">
-        <form className="space-y-4 p-5 bg-gray-200 mt-4">
-          <div className="flex items-center">
-            <label className="w-1/6">Avatar:</label>
-            <input type="file" className="w-2/6 bg-gray-100 p-2" />
-          </div>
-          <div className="flex items-center">
-            <label className="w-1/6">Tên cá nhân:</label>
-            <input type="text" className="w-2/6 bg-gray-100 p-2" />
-          </div>
-          <div className="flex items-center">
-            <label className="w-1/6">Địa chỉ:</label>
-            <input type="text" className="w-2/6 bg-gray-100 p-2" />
-          </div>
-          <div className="flex items-center">
-            <label className="w-1/6">Số điện thoại:</label>
-            <input type="text" className="w-2/6 bg-gray-100 p-2" />
-          </div>
-          <div className="flex items-center">
-            <label className="w-1/6">Email:</label>
-            <input type="email" className="w-2/6 bg-gray-100 p-2" />
-          </div>
-          <div className="w-full flex justify-end">
-             <button className="btn__seemore transition text-white px-4 py-2 mt-3">Cập nhật thông tin</button>
-          </div>
-        </form>
-      </div>
+     
     </div>
   );
 };
